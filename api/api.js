ff04f506bb26101f4ecd7cecb55c10f7cf51c95b
glaukiol1 <glaukiol1@gmail.com>
2022-03-12 21:08:36 +0100

    changes

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 const parseReq = require('../main');
 
 module.exports = (req, res) => {
-    const {username, title} = req.query.title
+    const {username, title} = req.query
     res.setHeader("Content-Type", "image/svg+xml")
     parseReq(username, title)
         .then(data => {
