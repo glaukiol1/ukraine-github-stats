43e15822bbd0c1655d8803302e793669e6547585
glaukiol1 <glaukiol1@gmail.com>
2022-03-12 21:17:28 +0100

    change font

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const parseReq = require('../main');
 
 module.exports = (req, res) => {
     const {username, title} = req.query
+    if (!username) {
+        return res.send("<h1>Invalid Paramaters</h1>")
+    }
     res.setHeader("Content-Type", "image/svg+xml")
     parseReq(username, title)
         .then(data => {
