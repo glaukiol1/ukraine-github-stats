876b0e072ad57b33c8160eaf07d289ba5e8d1c76
glaukiol1 <glaukiol1@gmail.com>
2022-03-12 21:03:13 +0100

    changes

--- api/api.js
+++ api/api.js
@@ -1,10 +1,7 @@
-const express = require('express');
 const parseReq = require('../main');
-const app = express();
 
-app.get('/api/:username', (req,res)=>{
-    const { username } = req.params
-    const title = req.query.title
+module.exports = (req, res) => {
+    const {username, title} = req.query.title
     res.setHeader("Content-Type", "image/svg+xml")
     parseReq(username, title)
         .then(data => {
@@ -14,10 +11,4 @@ app.get('/api/:username', (req,res)=>{
             res.setHeader("Content-Type", "plain/text")
             res.send("ERROR!").status(500)
         })
-})
-
-app.get('*', (req,res) => {
-    res.send("Hello, this page was not found.")
-})
-
-module.exports = app
+}
